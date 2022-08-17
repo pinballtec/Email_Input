1a2ee94e38144725ed8876c7adfae62709d857a8
Daniil_Boika <daniilboiko99@gmail.com>
2022-08-17 12:06:49 +0200

    mailchimp has been set up

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let request = require('request');
+let https = require('https');
 
 port = 8000;
 let app = express();
@@ -16,9 +17,43 @@ app.post('/', function(req, res){
     let name = req.body.name;
     let last_name = req.body.last_name;
     let email = req.body.email;
-    res.send(name +  " " + last_name +  " " + email);
-})
+    // res.send(name +  " " + last_name +  " " + email);
+    let data = {
+        members: [
+        {
+            email_address: email,
+            status: "subscribed",
+            merge_fields : {
+                FNAME: name,
+                LNAME: last_name,
+            }
+        }
+      ]
+    };
+
+    let jsonData = JSON.stringify(data);
+
+    let url = '';
+
+    let options = {
+        method: 'POST',
+        auth: '',
+    }
+
+    let request = https.request(url, options, function(response){
+        response.on('data', function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
 
 app.listen(port, () => {
     console.log('Listening on port 8000');
 });
+
+
+// 
